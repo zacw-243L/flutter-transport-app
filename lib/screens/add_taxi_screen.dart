e0bf8e037bf5dafdcd08e83a939eaf8570f672c1
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-07-10 16:38:31 +0800

    Added the ability to add to database

--- lib/screens/add_taxi_screen.dart
+++ lib/screens/add_taxi_screen.dart
@@ -14,13 +14,43 @@ class AddTaxiScreen extends StatefulWidget {
 
 class _AddTaxiScreenState extends State<AddTaxiScreen> {
   FirebaseAuth auth = FirebaseAuth.instance;
-  CollectionReference tasksCollection =
+  CollectionReference faresCollection =
       FirebaseFirestore.instance.collection('fares');
 
   final TextEditingController originController = TextEditingController();
   final TextEditingController destController = TextEditingController();
   final TextEditingController fareController = TextEditingController();
   final TextEditingController dateController = TextEditingController();
+
+  Future<void> _addfares(
+      String newOrigin, String newDest, double fare, DateTime date) {
+    return faresCollection
+        .add({
+          'origin': newOrigin,
+          'dest': newDest,
+          'fare': fare,
+          'date': date,
+          'userid':
+              auth.currentUser?.uid ?? '', // Ensure non-null value for userid
+        })
+        .then((value) => print("Taxi fare added"))
+        .catchError((error) => print("Failed to add taxi fare: $error"));
+  }
+
+  void _handleAdd() {
+    String origin = originController.text;
+    String dest = destController.text;
+    double fare = double.tryParse(fareController.text) ?? 0.0;
+    DateTime date = DateTime.tryParse(dateController.text) ?? DateTime.now();
+
+    _addfares(origin, dest, fare, date).then((_) {
+      // Clear the text fields after successful addition
+      originController.clear();
+      destController.clear();
+      fareController.clear();
+      dateController.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +76,12 @@ class _AddTaxiScreenState extends State<AddTaxiScreen> {
           ),
           TextField(
             textAlign: TextAlign.center,
-            decoration: const InputDecoration(labelText: 'Date'),
+            decoration: const InputDecoration(labelText: 'Date (YYYY-MM-DD)'),
             controller: dateController,
           ),
           SizedBox(height: 20), // Add some space before the button
           ElevatedButton(
-            onPressed: () {}, // Empty onPressed callback
+            onPressed: _handleAdd,
             child: const Text('Add'),
           ),
         ],
